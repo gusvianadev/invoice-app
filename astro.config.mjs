93bc481e502f404451e1af269ba3be668c7da238
gusvianadev <gusvianadev@gmail.com>
2023-09-10 21:25:28 -0300

    vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({
-    applyBaseStyles: false
-  })],
-  output: "hybrid",
-  adapter: node({
-    mode: "standalone"
-  })
+	integrations: [
+		react(),
+		tailwind({
+			applyBaseStyles: false,
+		}),
+	],
+	output: "hybrid",
+	adapter: vercel(),
 });
